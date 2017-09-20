9e524dd7ce593ef1c079329201ae0c40e8fcba64
minz3006 <31067842+minz3006@users.noreply.github.com>
2017-09-20 23:25:58 +0200

    Add files via upload

---
+++ Calculator_Project/Calculator_Project/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Perimeter_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void Pythagoras_button_Click(object sender, RoutedEventArgs e)
+        {
+         
+           
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (radioButton.IsChecked == true)
+            {
+                Window1 a = new Window1();
+                a.Show();
+            }
+        }
+    }
+}
